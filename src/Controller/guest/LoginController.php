6775bceb26f0038faab846af5b3d56ecac1d3bc0
Laurence Renevier <Laurence.renevier@Lapiscine.pro>
2025-05-12 16:40:36 +0200

    create-user

---
+++ src/Controller/guest/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace App\Controller\guest;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController {
+
+#[Route('/login', name: "login")]
+	public function displayLogin(AuthenticationUtils $authenticationUtils) {
+
+		$error = $authenticationUtils->getLastAuthenticationError();
+
+		return $this->render('guest/login.html.twig', [
+			'error' => $error
+		]);
+
+	}
+
+    #[Route('/logout', name: "logout")]
+	public function logout() {
+
+	}
+
+}
